b1d535b997327db9f9e6d6ce7ef27099623928ec
Ingrdst <ingr.dcg@gmail.com>
2018-06-19 00:37:48 -0300

    Tela inicial, cadastro e rotas

--- project/config/routes.rb
+++ project/config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+
+  root 'start#index'
+  
+  resources :users
+
+  get 'opensound' => 'start#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
