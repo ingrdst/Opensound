3ef9ff9fefc2c58056a8798e5d7191ac64210d42
Ingrdst <ingr.dcg@gmail.com>
2018-06-25 16:22:04 -0300

    Add Track Model & Associations

--- project/db/schema.rb
+++ project/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_25_055134) do
+ActiveRecord::Schema.define(version: 2018_06_25_191529) do
 
   create_table "sounds", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,6 +29,15 @@ ActiveRecord::Schema.define(version: 2018_06_25_055134) do
     t.text "description"
     t.index ["email"], name: "index_sounds_on_email", unique: true
     t.index ["reset_password_token"], name: "index_sounds_on_reset_password_token", unique: true
+  end
+
+  create_table "tracks", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "sound_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["sound_id"], name: "index_tracks_on_sound_id"
   end
 
 end
